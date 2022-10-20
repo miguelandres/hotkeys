9d5347fd591cfb413b5bb53976e98a4b7923cc72
Miguel Barreto <miguelandres@users.noreply.github.com>
2022-10-20 16:57:33 -0400

    Remove read-only stuff

--- boot.py
+++ boot.py
@@ -4,10 +4,10 @@ import storage
 
 # Mount the CIRCUITPY drive as read-only unless
 # the encoder switch is held down
-encoder_switch = digitalio.DigitalInOut(board.BUTTON)
-encoder_switch.switch_to_input(pull=digitalio.Pull.UP)
-if(encoder_switch.value):
-    print("Mounting Read-Only")
-else:
-    print("Mounting Read/Write")
-storage.remount("/", not encoder_switch.value)
+# encoder_switch = digitalio.DigitalInOut(board.BUTTON)
+# encoder_switch.switch_to_input(pull=digitalio.Pull.UP)
+# if(encoder_switch.value):
+#     print("Mounting Read-Only")
+# else:
+#     print("Mounting Read/Write")
+# storage.remount("/", not encoder_switch.value)
