52ab86934c21e99243a5a7a6d1b1f4c432574e92
Miguel Barreto <miguelandres@users.noreply.github.com>
2024-01-23 23:54:55 -0500

    Add a new_app module, still not working

---
+++ new_app.py
@@ -0,0 +1,81 @@
+# # License and Copyright
+#
+# Copyright for portions of this project are held by John Ellis, 2001 as part of
+# deckerego/Macropad_Hotkeys, licensed under an MIT license.
+#
+# All other copyright for in this project is held by Miguel Barreto, 2022, also
+# licensed under an MIT License
+#
+# ## MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""This module declares the App class which can declare different behaviors of
+the macropad """
+from typing import Callable
+from action import Action, MediaControl, Sequence
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from button import Button
+
+
+class App:
+  """Class that defines the behavior of each button for a particular macropad
+  configuration"""
+
+  def __init__(
+      self,
+      title: str | Callable[[], str],
+      buttons: list[Button],
+      encoder_increase_actions: list[int, str, Action] | Action =
+          MediaControl(ConsumerControlCode.VOLUME_INCREMENT),
+      encoder_decrease_actions: list[int, str, Action] | Action = MediaControl(
+          ConsumerControlCode.VOLUME_DECREMENT)
+  ):
+    self._title: str | Callable[[], str] = title
+    self._buttons: list[Button] = buttons
+    if len(self._buttons) != 12:
+      raise Exception('App {self.title} does not contain 12 buttons!')
+    self._encoder_increase_action: Action = (
+        encoder_increase_actions if isinstance(encoder_increase_actions, Action)
+        else Sequence(encoder_increase_actions)
+    )
+    self._encoder_decrease_action: Action = (
+        encoder_decrease_actions if isinstance(encoder_decrease_actions, Action)
+        else Sequence(encoder_decrease_actions)
+    )
+
+  @property
+  def title(self) -> str:
+    return self._title if isinstance(self._title, str) else self._title()
+
+  @property
+  def buttons(self) -> str:
+    return self._buttons
+
+  @property
+  def button_titles(self) -> list[str]:
+    return map(self._buttons, Button.title)
+
+  @property
+  def encoder_increase_action(self) -> Action:
+    return self._encoder_increase_action
+
+  @property
+  def encoder_decrease_action(self) -> Action:
+    return self._encoder_decrease_action
