cbaf7cf759aabe7a02a1d7e0ee891a5c12b0f24d
Miguel Barreto <miguelandres@users.noreply.github.com>
2024-01-24 23:31:24 -0500

    Modify main code to call into new controller

--- code.py
+++ code.py
@@ -27,79 +27,10 @@
 # SOFTWARE.
 
 """Entry point to the code for the Hotkeys program"""
-from hotkeys.action import Sequence
-from adafruit_macropad import MacroPad
-from app import App, load_all_apps
-from display import Display
-from pixels import Pixels
-from keypad import Event
 
-MACRO_FOLDER = '/macros'
+from hotkeys.controller import Controller
 
-macropad: MacroPad = MacroPad()
-screen: Display = Display(macropad)
-pixels: Pixels = Pixels(macropad)
-last_position: int = 0
-switching_mode: bool = False
-app_index: int = 0
+controller = Controller()
 
-screen.initialize()
-
-
-apps: list[App] = load_all_apps(MACRO_FOLDER)
-
-if not apps:
-  screen.set_title('NO MACRO FILES FOUND')
-  while True:
-    pass
-
-try:  # Test the USB HID connection
-  macropad.keyboard.release_all()
-except OSError as err:
-  print(err)
-  screen.set_title('NO USB CONNECTION')
-  while True:
-    pass
-
-
-def set_app(new_app_index: int):
-  global app_index
-  app_index = new_app_index % len(apps)
-  macropad.keyboard.release_all()
-  screen.set_app(apps[app_index])
-  pixels.set_app(apps[app_index])
-
-
-last_position = 0
-set_app(0)
-
-while True:
-  macropad.encoder_switch_debounced.update()
-  if macropad.encoder_switch_debounced.released:
-    switching_mode = not switching_mode
-  position: int = macropad.encoder
-  if position != last_position:
-    if switching_mode:
-      set_app(position)
-    else:
-      if position > last_position:
-        apps[app_index].encoder_increase_action.run(macropad)
-      else:
-        apps[app_index].encoder_decrease_action.run(macropad)
-    last_position = position
-  event: Event | None = macropad.keys.events.get()
-  if not event or event.key_number >= len(apps[app_index].macros):
-    continue  # No key events, or no corresponding macro, resume loop
-  key_number: int = event.key_number
-  pressed: bool = event.pressed
-
-  sequence = Sequence(apps[app_index].macros[key_number]
-                      [2] if key_number < 12 else [])
-  if pressed:
-    if key_number < 12:
-      pixels.highlight(key_number, 0xFFFFFF)
-    sequence.begin(macropad)
-
-  else:
-    sequence.end(macropad)
-    pixels.reset(key_number)
+controller.initialize()
+controller.run_loop()
